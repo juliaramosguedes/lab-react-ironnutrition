20b78c4b60e65e77fc65eba0989dd1eed1140ab7
Julia Ramos <juliaramosguedes@gmail.com>
2019-10-31 22:38:20 -0300

    feats: add new food and filter foods

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Title, Button, FilterForm, FoodBox, TodaysFood, FormAddFood } from './components';
+import foods from './foods.json';
 class App extends Component {
+  state = {
+    foods: [],
+    filteredFoods: [],
+  }
+
+  componentDidMount() {
+    this.setState({ 
+      foods: [...foods], 
+      filteredFoods: [...foods],
+    });
+  }
+
+  filteredFoods = (filter) => {
+    const { foods } = this.state;
+    const { name } = filter;
+
+    const filteredFoods = foods.filter((food) => {
+      return food.name.toLowerCase().includes(name)
+    });
+
+    this.setState({
+      filteredFoods,
+    })
+  }
+
+  displayFood = () => {
+    const { filteredFoods } = this.state;
+
+    return filteredFoods.map((food, idx) => {
+      const { name, image, calories } = food;
+      return(
+        // {console.log('retornou displayfood')}
+        <FoodBox name={food.name} image={food.image} calories={food.calories} />
+      )
+    })
+  }
+
+  handleSubmit = (food) => {
+    const { foods, filteredFoods } = this.state;
+    foods.push(food);
+    filteredFoods.push(food);
+    this.setState({ foods, filteredFoods });
+  }
+
   render() {
+    const { showForm } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title htmlType="H1">IronNutrition</Title>
+        <FilterForm filterFunction={this.filteredFoods} />
+        <FormAddFood addFoodFunction={this.handleSubmit} />
+        <div className="container">
+          <div className="foods-list">
+            {this.displayFood()}
+          </div>
+          <div>
+            <TodaysFood />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ starter-code/src/components/TodaysFood/TodaysFood.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TodaysFood.css';
+
+class TodaysFood extends Component {
+
+  render() {
+    return(
+      <p>oi</p>
+    )
+  }
+}
+
+export default TodaysFood;
